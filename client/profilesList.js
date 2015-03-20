d8bbb89aec85eacd86a2448a6120dbcf1adbc1a4
Thabang Tseboho <tseboho@gmail.com>
2015-03-20 11:15:15 +0200

    renamed templates. Added count template helper.

--- client/profilesList.js
+++ client/profilesList.js
@@ -3,5 +3,8 @@ Meteor.subscribe('profiles');
 Template.profilesList.helpers({
 	profilesList: function() {
 		return Profiles.find();
+	},
+	profilesCount: function() {
+		return Profiles.find().count();
 	}
 });
