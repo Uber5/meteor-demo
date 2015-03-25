77f5d2b9f0b9822697b9b1f75ed17562eb1ddce1
Thabang Tseboho <tseboho@gmail.com>
2015-03-25 10:30:31 +0200

    ongoing: experimenting with oauth.

--- server/main.js
+++ server/main.js
@@ -9,7 +9,7 @@ if (Meteor.isServer) {
       clientId: '57d785fc-1df4-42a4-aca4-21b1439bc867',
       loginStyle: 'redirect',
       secret: 'AMROz0pBU2Ae9oQchzjM9CBpuwA5jhH-8ec9N6cpwKfE_ybeb9tb4_3YeqsOzgj78OeMXYqaTwK5BAjtsEGM3MQ',
-      issuer: 'https://authenticate.mytimeslot.co.za',
+      issuer:  'https://authenticate.mytimeslot.co.za',
       requestPermissions: [ 'email', 'userinfo', 'openid' ]
     });   
   });
