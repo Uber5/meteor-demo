dd35731976ec418e3677fe8dff49d4e6bee5f1a4
Chris Oloff <christian.oloff@gmail.com>
2015-03-30 19:15:45 +0200

    added missing 'JSON.parse', added displaying the name of an item

--- server/publish.js
+++ server/publish.js
@@ -2,15 +2,16 @@ Meteor.publish('profiles', function() {
 	return Profiles.find();
 });
 
-
 var refreshItems = function() {
   var response = Meteor.http.get("http://localhost:8000/api/profiles");
-  var items = response.content;
+  var items = JSON.parse(response.content);
+  console.log('response.content', items);
   Items.remove({
   	id: { $nin: _.map(items, function(item) { return item.id; })}
   });
   _.each(items, function(item) {
-    Items.upsert({ id: +item.id }, 
+    console.log('about to upsert item:', item);
+    Items.upsert({ id: +item.id },
     {
       id: +item.id,
       name: item.first_name,
