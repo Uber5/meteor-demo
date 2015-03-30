2e6a5e31be6b5b0061c06c0f1bdcd0387e8cda02
Thabang Tseboho <tseboho@gmail.com>
2015-03-30 19:59:19 +0200

    prune console logs.

--- server/publish.js
+++ server/publish.js
@@ -2,15 +2,18 @@ Meteor.publish('profiles', function() {
 	return Profiles.find();
 });
 
+Meteor.publish('items', function() {
+		refreshItems();
+		return Items.find();
+});
+
 var refreshItems = function() {
   var response = Meteor.http.get("http://localhost:8000/api/profiles");
   var items = JSON.parse(response.content);
-  console.log('response.content', items);
   Items.remove({
   	id: { $nin: _.map(items, function(item) { return item.id; })}
   });
   _.each(items, function(item) {
-    console.log('about to upsert item:', item);
     Items.upsert({ id: +item.id },
     {
       id: +item.id,
@@ -18,8 +21,3 @@ var refreshItems = function() {
     });
   });
 }
-
-Meteor.publish('items', function() {
-		refreshItems();
-		return Items.find();
-});
